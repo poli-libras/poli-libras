ef69969cee187dd159285c083256cfcbf8cdc3f9
leonardofl <leonardofl87@gmail.com>
2013-04-10 22:39:00 -0300

    ManualTranslatorTester pra ajudar testes manuais pra encontrar bugs

---
+++ poli-libras-core/test/br/usp/libras/translator/ManualTranslatorTester.java
@@ -0,0 +1,24 @@
+package br.usp.libras.translator;
+
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import br.usp.libras.sign.Sign;
+public class ManualTranslatorTester {
+
+	public static void main(String[] args) {
+
+    	Translator translator = new Translator();
+    	
+    	while (true) {
+    		String input = JOptionPane.showInputDialog("Digite sua frase:");
+    		List<Sign> signs = translator.translate(input);
+    		System.out.println("------------------------s");
+    		System.out.println("Traducao para frase:" + input);
+    		for (Sign sign : signs) {
+				System.out.println(sign.toString());
+			}
+    	}
+	}
+}
